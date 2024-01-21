91d54421cad508558489069db01009acd88e2c97
Aml <aml.hassan.esmil@gmail.com>
2024-01-21 18:18:14 +0200

    modify load function

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import streamlit as st
 from streamlit_image_select import image_select
+from st_audiorec import st_audiorec
 import torch
 
 from wav2lip import inference 
 from wav2lip.models import Wav2Lip
+
 device='cpu'
 #@st.cache_data is used to only load the model once
 @st.cache_data 
 def load_model(path):
 	model = Wav2Lip()
 	print("Load checkpoint from: {}".format(path))
-	if device == 'cuda':
-		checkpoint = torch.load(path)
-	else:
-		checkpoint = torch.load(path,
-								map_location=lambda storage, loc: storage)
+	checkpoint = torch.load(path,map_location=lambda storage, loc: storage)
 	s = checkpoint["state_dict"]
 	new_s = {}
 	for k, v in s.items():
@@ -35,8 +33,15 @@ def streamlit_look():
     """
     data={}
     st.title("Welcome to AI Lip Sync :)")
-    avatar_img = image_select("Choose your Avatar", ["avatars_images/avatar1.jpg"])
-    data["imge_path"]=avatar_img
+    avatar_img = image_select("Choose your Avatar", 
+							  ["avatars_images/avatar1.jpg"])
+    data["imge_path"] = avatar_img
+    wav_audio_data = st_audiorec()
+    col_playback, col_space = st.columns([0.58,0.42])
+    with col_playback:
+            st.audio(wav_audio_data, format='audio/wav')
+    #if wav_audio_data is not None:
+    #      st.audio(wav_audio_data, format='audio/wav')
     return data
 
 def main():  
@@ -44,9 +49,9 @@ def main():
     fast_animate = st.button("fast animate")
     slower_animate = st.button("slower animate")
     if fast_animate:
-        inference.main('wav2lip/checkpoints/wav2lip_gan.pth',data["imge_path"],"AI4.wav",model) 
+        inference.main('wav2lip/checkpoints/wav2lip_gan.pth',data["imge_path"],"sound.wav",model) 
     if slower_animate:
-        inference.main('wav2lip/checkpoints/wav2lip_gan.pth',"avatars/w.mp4","AI4.wav",model)  
+        inference.main('wav2lip/checkpoints/wav2lip_gan.pth',image_video_map[data["imge_path"]],"sound.wav",model)  
 
 if __name__ == "__main__":
     main()
